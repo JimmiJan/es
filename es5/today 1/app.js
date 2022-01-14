9414906b2966ea3db328b963a6738ec733ca39f7
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-01-14 23:15:26 +0500

    first 1

---
+++ es5/today 1/app.js
@@ -0,0 +1,204 @@
+//  alert("Assalamualikum Man")
+
+
+// var names = "jamshed "
+// alert(names)
+
+// var num = 7
+// alert(num)
+
+
+// var boll = true
+// alert(boll)
+
+
+// var bool = false
+// alert(bool)
+
+
+// var undefi = " "
+// alert(undefi)
+
+
+// var names = "sahib"
+// console.log(names)
+
+
+
+// var nam = "sahib"
+// nam = "jimmi"
+// console.log(nam)
+
+
+// legal name
+
+
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+// response
+
+
+
+// var num1 = 33
+// var num2 = 55
+// alert (num1 + num2)
+
+
+
+
+
+// var num1 = 33
+// var num2 = 55
+// alert (num1 - num2)
+
+
+
+
+
+
+// var num1 = 33
+// var num2 = 55
+// alert (num1 * num2)
+
+
+
+
+
+// var num1 = 33
+// var num2 = 55
+// alert (num1 / num2)
+
+
+
+// var num1 = 10
+// var num2 = 3
+// alert (num1 % num2)
+
+
+// inc and dec
+
+
+// var num= 3
+// var newnum = num++ + ++num
+// alert(newnum)
+
+
+
+// var num= 3
+// var newnum = num-- + --num
+// alert(newnum)
+
+
+
+
+
+
+// var num= 3
+// var newnum = num++ + --num - ++num
+
+
+// alert(newnum)
+
+
+
+
+
+
+
+// Math expressions:
+// Eliminating ambiguity
+
+// var totalCost = 1 + 3 * 4;
+
+
+// alert(totalCost)
+
+
+// var totalCost = (1 + 3) * 4;
+
+
+// alert(totalCost)
+
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+
+// alert(resultOfComputation)
+
+
+
+
+
+
+
+
+// Concatenating text strings
+
+
+
+// var concate = "Thanks for your input!"
+
+// alert(concate)
+
+
+
+
+// var con = "thanks"
+// var num = 33
+// alert(con + num)
+
+
+// var con = "99"
+// var num = 33
+// alert(con + " " + num)
+
+
+
+//  var message = "Thanks, ";
+//  var userName = "Susan";
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess);
+
+
+
+
+// promt 
+
+
+// var names = prompt("Whats Your Name" , "Please Enter your name" )
+// alert(names)
+
+
+
+
+// var num1 = prompt("Whats Your favt num" , "Please Enter your num" )
+// var num2 = prompt("Whats Your favt num2" , "Please Enter your num2" )
+
+// alert(num1 + num2)
+
+
+
+// var num1 = +prompt("Whats Your favt num" , "Please Enter your num" )
+// var num2 = +prompt("Whats Your favt num2" , "Please Enter your num2" )
+
+// alert(num1 + num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
